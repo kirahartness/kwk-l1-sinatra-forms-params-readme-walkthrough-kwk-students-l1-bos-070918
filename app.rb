9b2ad7cfb6767b51e2f1b55b5fac8f4837a2605c
Kira Hartness <kirahartness@gmail.com>
2018-07-19 20:04:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+get '/' do 
+  "This is a home page"
+  
   get '/food_form' do
     erb :food_form
   end
+  
+  post '/food_form' do 
+    params.to_s 
+  end
 
-  # Add your post route and action below
-
-end
+  
