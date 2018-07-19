bf6044410da7cccc69fea49b40c3e816668be661
Kira Hartness <kirahartness@gmail.com>
2018-07-19 20:06:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,3 @@
-require_relative 'config/environment'
 
-class App < Sinatra::Base
-get '/' do 
-  "This is a home page"
-  
-  get '/food_form' do
-    erb :food_form
-  end
-  
-  post '/food_form' do 
-    params.to_s 
-  end
 
   
